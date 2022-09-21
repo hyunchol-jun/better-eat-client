19c968bceca8038d3ad6aed7ac381e07f04519a0
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-21 19:28:24 -0400

    Fix header to show login button on signup, login page

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,14 +1,18 @@
 import "./PageHeader.scss";
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 import menuIcon from "../../assets/icons/menu.svg";
 import IconButton from "../IconButton/IconButton";
 
 function PageHeader({handleClick}) {
+    const location = useLocation();
+
     return (
       <header className='page-header'>
         <IconButton className="page-header__menu" imgSrc={menuIcon} altText='hamburger icon' handleClick={handleClick}/>
         <span className='page-header__title'>BetterEat</span>
-        <Link className="page-header__auth" to="/login">Logout</Link>
+        <Link className="page-header__auth" to="/login">
+            {location.pathname === "/signup" || location.pathname === "/login" ? "Login" : "Logout"}
+        </Link>
       </header>
     );
 }
