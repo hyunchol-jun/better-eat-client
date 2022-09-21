0f7038ac2ecb934a533cc5c6aa03fb37d8b27f63
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-21 10:41:30 -0400

    Move recipes state from HomePage to App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import RecipeDetail from "./pages/RecipeDetail/RecipeDetail";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {useState} from "react";
+import {getRecipesList} from "./utils/http-helper";
 
 function App() {
   const [sidebarShown, setSidebarShown] = useState(false);
+  const [recipes, setRecipes] = useState(null);
+
+  const diets = ["balanced", "low-sodium"];
+  const healths = ["vegan", "celery-free"];
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+
+    const searchQuery = event.target.textInput.value;
+    const recipeCallback = (response) => {
+      setRecipes(response.data.hits);
+    };
+
+    getRecipesList(searchQuery, diets, healths, recipeCallback);
+  };
+
 
   const handleSidebarVisibility = () => {
     setSidebarShown(!sidebarShown);
@@ -18,7 +35,7 @@ function App() {
       <PageHeader handleClick={handleSidebarVisibility}/> 
       {sidebarShown && <Sidebar />}
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/" element={<HomePage recipes={recipes} handleSearch={handleSearch}/>}></Route>
         <Route path="/recipes" element={<RecipeDetail />}></Route>
       </Routes>
     </BrowserRouter>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,26 +2,8 @@ import "./HomePage.scss";
 import PreferenceBar from '../../components/PreferenceBar/PreferenceBar';
 import RecipesList from '../../components/RecipesList/RecipesList';
 import Search from '../../components/Search/Search';
-import {getRecipesList} from "../../utils/http-helper";
-import {useState} from "react";
 
-function HomePage() {
-  const [recipes, setRecipes] = useState(null);
-
-  const diets = ["balanced", "low-sodium"];
-  const healths = ["vegan", "celery-free"];
-
-  const handleSearch = (event) => {
-    event.preventDefault();
-
-    const searchQuery = event.target.textInput.value;
-    const recipeCallback = (response) => {
-      setRecipes(response.data.hits);
-    };
-
-    getRecipesList(searchQuery, diets, healths, recipeCallback);
-  };
-
+function HomePage({handleSearch, recipes}) {
 
   return (
     <main className="home-page">
