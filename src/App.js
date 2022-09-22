a44c199934c588d1d1f9d080f6bb63622d6f5e85
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-22 13:30:37 -0400

    Move preference states to App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,49 @@ import HomePage from './pages/HomePage/HomePage';
 import Sidebar from "./components/Sidebar/Sidebar";
 import RecipeDetail from "./pages/RecipeDetail/RecipeDetail";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {useState} from "react";
+import {useState, useRef, useEffect} from "react";
 import {getRecipesList} from "./utils/http-helper";
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import defaultHealthTypes from "./data/healthTypes";
+import defaultDietTypes from "./data/dietTypes";
 
 function App() {
   const [sidebarShown, setSidebarShown] = useState(false);
   const [recipes, setRecipes] = useState(null);
+
+  const healthTypesFromStorage = JSON.parse(localStorage.getItem("healthTypes"));
+  const dietTypesFromStorage = JSON.parse(localStorage.getItem("dietTypes"));
+  const [healthTypes, setHealthTypes] = useState(healthTypesFromStorage);
+  const [dietTypes, setDietTypes] = useState(dietTypesFromStorage);
+
+  // Reference to state value to get updated data
+  const healthTypesRef = useRef(); 
+  const dietTypesRef = useRef();
+
+  const handleHealthTypeChange = (typeName) => {
+    setHealthTypes((prevState) => {
+      const copiedHealthTypes = {...prevState};
+      copiedHealthTypes[typeName] = !copiedHealthTypes[typeName];
+      return copiedHealthTypes;
+    })
+  }
+
+  const handleDietTypeChange = (typeName) => {
+    setDietTypes((prevState) => {
+      const copiedDietTypes = {...prevState};
+      copiedDietTypes[typeName] = !copiedDietTypes[typeName];
+      return copiedDietTypes;
+    });
+  }
+
+  if (!healthTypes) {
+    setHealthTypes(defaultHealthTypes);
+  }
+
+  if (!dietTypes) {
+    setDietTypes(defaultDietTypes);
+  }
 
   const diets = ["balanced", "low-sodium"];
   const healths = ["vegan", "celery-free"];
@@ -27,6 +62,16 @@ function App() {
     getRecipesList(searchQuery, diets, healths, recipeCallback);
   };
 
+  // Cache state value to ref
+  useEffect(() => {
+    healthTypesRef.current = {...healthTypes};
+    localStorage.setItem("healthTypes", JSON.stringify(healthTypesRef.current));
+  }, [healthTypes]);
+
+  useEffect(() => {
+    dietTypesRef.current = {...dietTypes};
+    localStorage.setItem("dietTypes", JSON.stringify(dietTypes));
+  }, [dietTypes]);
 
   const handleSidebarVisibility = () => {
     setSidebarShown(!sidebarShown);
@@ -37,7 +82,14 @@ function App() {
       <PageHeader handleClick={handleSidebarVisibility}/> 
       {sidebarShown && <Sidebar />}
       <Routes>
-        <Route path="/" element={<HomePage recipes={recipes} handleSearch={handleSearch}/>}></Route>
+        <Route path="/" element={<HomePage 
+                                    recipes={recipes} 
+                                    handleSearch={handleSearch}
+                                    healthTypes={healthTypes}
+                                    dietTypes={dietTypes}
+                                    handleDietTypeChange={handleDietTypeChange}
+                                    handleHealthTypeChange={handleHealthTypeChange}
+                                />}></Route>
         <Route path="/recipes" element={<RecipeDetail />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,63 +2,24 @@ import "./HomePage.scss";
 import PreferenceBar from '../../components/PreferenceBar/PreferenceBar';
 import RecipesList from '../../components/RecipesList/RecipesList';
 import Search from '../../components/Search/Search';
-import { useEffect, useState, useRef } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import defaultHealthTypes from "../../data/healthTypes";
-import defaultDietTypes from "../../data/dietTypes";
 
-function HomePage({handleSearch, recipes}) {
+function HomePage({
+                    handleSearch, 
+                    recipes, 
+                    handleDietTypeChange, 
+                    handleHealthTypeChange,
+                    dietTypes,
+                    healthTypes
+                  }) {
   const navigate = useNavigate();
-  const healthTypesFromStorage = JSON.parse(localStorage.getItem("healthTypes"));
-  const dietTypesFromStorage = JSON.parse(localStorage.getItem("dietTypes"));
-  const [healthTypes, setHealthTypes] = useState(healthTypesFromStorage);
-  const [dietTypes, setDietTypes] = useState(dietTypesFromStorage);
-
-  // Reference to state value to get updated data
-  const healthTypesRef = useRef(); 
-  const dietTypesRef = useRef();
-
-  const handleHealthTypeChange = (typeName) => {
-    setHealthTypes((prevState) => {
-      const copiedHealthTypes = {...prevState};
-      copiedHealthTypes[typeName] = !copiedHealthTypes[typeName];
-      return copiedHealthTypes;
-    })
-  }
-
-  const handleDietTypeChange = (typeName) => {
-    setDietTypes((prevState) => {
-      const copiedDietTypes = {...prevState};
-      copiedDietTypes[typeName] = !copiedDietTypes[typeName];
-      return copiedDietTypes;
-    });
-  }
-
-  if (!healthTypes) {
-    setHealthTypes(defaultHealthTypes);
-  }
-
-  if (!dietTypes) {
-    setDietTypes(defaultDietTypes);
-  }
-
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
       navigate("/login");
     }
   }, []);
-
-  // Cache state value to ref
-  useEffect(() => {
-    healthTypesRef.current = {...healthTypes};
-    localStorage.setItem("healthTypes", JSON.stringify(healthTypesRef.current));
-  }, [healthTypes]);
-
-  useEffect(() => {
-    dietTypesRef.current = {...dietTypes};
-    localStorage.setItem("dietTypes", JSON.stringify(dietTypes));
-  }, [dietTypes]);
 
   return (
     <main className="home-page">
