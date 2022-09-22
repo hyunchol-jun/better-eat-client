34a444d5703b6913c94fe1c25ddcc30f177cceae
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-21 21:51:51 -0400

    Add default preference data and load in homepage

---
+++ src/data/dietTypes.js
@@ -0,0 +1,12 @@
+const defaultDietTypes = [
+    {
+        "balanced": false,
+        "high-fiber": false,
+        "high-protein": false,
+        "low-carb": false,
+        "low-fat": false,
+        "low-sodium": false,
+    }
+];
+
+export default defaultDietTypes;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,22 +2,41 @@ import "./HomePage.scss";
 import PreferenceBar from '../../components/PreferenceBar/PreferenceBar';
 import RecipesList from '../../components/RecipesList/RecipesList';
 import Search from '../../components/Search/Search';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import defaultHealthTypes from "../../data/healthTypes";
+import defaultDietTypes from "../../data/dietTypes";
 
 function HomePage({handleSearch, recipes}) {
   const navigate = useNavigate();
+  const healthTypesFromStorage = JSON.parse(localStorage.getItem("healthTypes"));
+  const dietTypesFromStorage = JSON.parse(localStorage.getItem("dietTypes"));
+  const [healthTypes, setHealthTypes] = useState(healthTypesFromStorage);
+  const [dietTypes, sestDietTypes] = useState(dietTypesFromStorage);
+
+  if (!healthTypes) {
+    setHealthTypes(defaultHealthTypes);
+  }
+
+  if (!dietTypes) {
+    sestDietTypes(defaultDietTypes);
+  }
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
       navigate("/login");
     }
+
+    return () => {
+      localStorage.setItem("healthTypes", JSON.stringify(healthTypes));
+      localStorage.setItem("dietTypes", JSON.stringify(dietTypes));
+    }
   }, []);
 
   return (
     <main className="home-page">
-      <PreferenceBar />
+      <PreferenceBar healthTypes={healthTypes} dietTypes={dietTypes}/>
       <Search handleSearch={handleSearch}/>
       <RecipesList recipes={recipes}/>
     </main>
