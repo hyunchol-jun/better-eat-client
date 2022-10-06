0e2b656db1a6ad30ad91af49ceeb7bd0635e0e3e
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-10-06 17:58:17 -0400

    Fix minimum page heights

--- src/components/PageMain.js
+++ src/components/PageMain.js
@@ -2,15 +2,17 @@ import styled from "styled-components";
 
 const StyledMain = styled.main`
     margin: 0 1rem;
-    min-height: calc(100vh - 7rem);
+    min-height: calc(100vh - 8.5rem);
 
     @media (min-width: 768px) {
         margin: 1rem 2rem 1rem 14rem;
+        min-height: calc(100vh - 11rem);
     }
 
     @media (min-width: 1280px) {
         max-width: 52rem;
         margin: 3rem auto 3rem;
+        min-height: calc(100vh - 15rem);
     }
 `;
 
