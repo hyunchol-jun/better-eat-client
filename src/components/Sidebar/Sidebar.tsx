49182779c1113d90e27bdd80cf8e2c65b7b0208b
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-12-11 10:51:57 -0500

    Fix NavLink bug in RecipeDetail page sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -43,6 +43,7 @@ function Sidebar({
               isActive ? "sidebar__link sidebar__link--active" : "sidebar__link"
             }
             to="/recipes"
+            end
           >
             My Recipes
           </NavLink>
